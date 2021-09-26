038ed28d1e8a6c2bef77709028b3c3f37832e962
littone39 <63129949+littone39@users.noreply.github.com>
2021-09-26 17:10:12 -0500

    changed bookstests try 2

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -18,6 +18,38 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(len(authors) == 1)
         self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
 
+    #tests whether the tiebreak of first name works
+    def test_author_tiebreak(self):
+        # self.data_source = shorter source
+        #self.assertEqual(self.data_source.authors("bronte"), )
+        pass
+
+    #tests with incorrect type for search_test
+    def test_author_inttype(self):
+        self.assertRaises(ValueError, self.data_source.authors, 1)
+
+    def test_author_no_text(self):
+        self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
+        self.assertTrue(self.data_source.authors() == [])
+
+    def test_author_search(self):
+        self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
+        authors = self.data_source.authors("c")
+        self.assertTrue(authors[0].surname == "Christie")
+        self.assertTrue(authors[2].surname == "Willis")
+
+    def test_years_start(self):
+        self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
+        books = self.data_source.books_between_years(1987)
+        self.assertTrue(books[0].title == "Beloved")
+        self.assertTrue(len(books) == 3)
+        self.assertTrue(books[2].title == "Blackout")
+
+    def test_no_years(self):
+        self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
+        books = self.data_source.books_between_years()
+        self.assertTrue(len(books) == 5)
+
 if __name__ == '__main__':
     unittest.main()
 
