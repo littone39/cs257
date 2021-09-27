ddff06b42a380f804836a7b270be29bbabfc1d0b
littone39 <63129949+littone39@users.noreply.github.com>
2021-09-27 10:55:47 -0500

    added non-eq tests

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -20,6 +20,12 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(len(authors) == 1)
         self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
 
+    def test_author_search(self):
+        self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
+        authors = self.data_source.authors()
+        self.assertTrue(len(authors) == 5)
+        self.assertTrue(authors[0] == Author('Christie', 'Agatha'))
+
     #tests whether the tiebreak of first name works
     def test_author_tiebreak(self):
         self.data_source = booksdatasource.BooksDataSource('booksTest2.csv')
@@ -32,13 +38,7 @@ class BooksDataSourceTester(unittest.TestCase):
     def test_author_inttype(self):
         self.assertRaises(ValueError, self.data_source.authors, 1)
 
-    def test_author_no_text(self):
-        self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
-        authors = self.data_source.authors()
-        self.assertTrue(len(authors) == 5)
-        self.assertTrue(authors[0] == Author('Christie', 'Agatha'))
-
-    def test_author_search(self):
+    def test_author_text_search(self):
         self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
         authors = self.data_source.authors("c")
         self.assertTrue(authors[0] == Author('Christie', 'Agatha'))
@@ -46,22 +46,40 @@ class BooksDataSourceTester(unittest.TestCase):
 
     def test_book_search(self):
         self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
+        books = self.data_source.books()
+        self.assertTrue(len(books) == 5)
+        self.assertTrue(books[3] == Book('Blackout'))
+
+    def test_book_text_search(self):
+        self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
         books = self.data_source.books("b")
-        self.assertTrue(books[0].title == "Beloved")
-        self.assertTrue(books[1].title == "Blackout")
+        self.assertTrue(books[0] == Book("Beloved"))
+        self.assertTrue(books[1] == Book("Blackout"))
 
     def test_book_default_sort(self):
         self.data_source = booksdatasource.BooksDataSource('booksTest2.csv')
         books = self.data_source.books("w", "table")
-        self.assertTrue(books[0].title == "A Wild Sheep Chase")
-        self.assertTrue(books[1].title == "The Tenant of Wildfell Hall")
+        self.assertTrue(books[0] == Book("A Wild Sheep Chase"))
+        self.assertTrue(books[1] == Book("The Tenant of Wildfell Hall"))
+
+    def test_book_year_sort(self):
+        self.data_source = booksdatasource.BooksDataSource('booksTest2.csv')
+        books = self.data_source.books("th", "year")
+        self.assertTrue(books[0] == Book('Wuthering Heights'))
+        self.assertTrue(books[1] == Book('Thief of Time'))
+
+    def test_years_inclusive(self):
+        self.data_source = booksdatasource.BooksDataSource('booksTest2.csv')
+        books = books = self.data_source.books_between_years(1847, 1848)
+        self.assertTrue(books[0] == Book('Wuthering Height'))
+        self.assertTrue(books[0] == Book('The Tenant of Wildfell Hall'))
 
     def test_years_start(self):
         self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
         books = self.data_source.books_between_years(1987)
-        self.assertTrue(books[0].title == "Beloved")
+        self.assertTrue(books[0] == Book("Beloved"))
         self.assertTrue(len(books) == 3)
-        self.assertTrue(books[2].title == "Blackout")
+        self.assertTrue(books[2] == Book("Blackout"))
 
     def test_no_years(self):
         self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
@@ -71,7 +89,10 @@ class BooksDataSourceTester(unittest.TestCase):
     def test_no_start(self):
         self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
         books = self.data_source.books_between_years(None, 2000)
-        self.assertTrue(books[1].surname == "Christie")
+        self.assertTrue(books[1] == Book("And Then There Were None"))
+    
+    def test_years_input(self):
+        self.assertRaises(ValueError, self.data_source.books_between_years, "title")
 
     def test_same_book(self):
         self.data_source = booksdatasource.BooksDataSource('booksTest.csv')
