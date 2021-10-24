0ce53cf3fd0c3c0c2d76ce0a59f3f9d65c1f25b0
littone39 <63129949+littone39@users.noreply.github.com>
2021-10-24 13:16:39 -0500

    deleted note to self comment

--- olympics/olympics-schema.sql
+++ olympics/olympics-schema.sql
@@ -4,43 +4,7 @@ October 14, 2021
 CS257: Software Design
 
 This file contains commands to create tables from olympic data in sql. 
-Edits:
-- take height and weight out of athletes and put it in events 
-- abstract olympic events
-- have one table with PERFORMANCES ? 
-
-noc_countries (noc, country)
-athletes (id, name, sex)
-events (id, sport, event)
-games (id, season, year, city)
-performances (athlete_id, event_id, games_id, medal, height, weight, noc)
-
-SELECT athletes.name, noc_countries.country
-FROM athletes, performances, noc_countries
-WHERE performances.athlete_id = athletes.id
-AND performances.noc = noc_countries.noc
-AND noc_countries.country LIKE 'Kenya'
-GROUP BY athletes.name
-SORT BY athletes.surname;
-
-SELECT athletes.name, performances.medal ...
-FROM athletes, performances, games
-WHERE athlete.surname LIKE 'Louganis'
-AND athletes.id = performances.athlete_id
-AND performances.medal LIKE '%'
-AND performances.games_id = games.id
-SORT BY games.year;
-
-SELECT performances.noc, COUNT(performances.medal)
-FROM performances,
-WHERE performances.medal LIKE 'Gold'
-GROUP BY performances.noc
-SORT BY COUNT(performances.noc) DESC;
-
-Questions:
-- should i be looping through multiple times in convert.py? or is it fine that I'm just doing it all at once?
-- what do you think of the above code
-- when I was doing this, I thought it was counter intuitive bc it seems harder to query. should I be trying to 
+ 
 */
 
 CREATE TABLE noc_countries (
