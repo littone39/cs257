f0636894d45b423f0f490aaada0f007302f1c767
littone39 <63129949+littone39@users.noreply.github.com>
2021-10-28 19:35:08 -0500

    cleanup of comments and formatting

--- olympics/olympics-api.py
+++ olympics/olympics-api.py
@@ -12,15 +12,15 @@ import psycopg2
 import argparse
 from config import database, user, password
 
-# Establishing a connection to the database
+app = flask.Flask(__name__)
+
+# establishing a connection to the olympic database
 try:
     connection = psycopg2.connect(database=database, user=user, password=password)
     cursor = connection.cursor()
 except Exception as e:
     print(e)
     exit()
-
-app = flask.Flask(__name__)
 
 @app.route('/games')
 def get_games():
@@ -32,12 +32,10 @@ def get_games():
     except Exception as e:
         print(e)
         exit()
-
     games_list = []
     for game in cursor:
         game_info = {'id':int(game[0]), 'year':int(game[1]), 'season':game[2], 'city':game[3]}
         games_list.append(game_info)
-    
     return json.dumps(games_list)
 
 @app.route('/nocs')
@@ -51,12 +49,10 @@ def get_nocs():
     except Exception as e:
         print(e)
         exit()
-
     nocs_list = []
     for noc in cursor:
         noc_info = {'abreviation':noc[0], 'name':noc[1]}
         nocs_list.append(noc_info)
-    
     return json.dumps(nocs_list)
 
 @app.route('/medalists/games/<games_id>')
@@ -67,44 +63,45 @@ def get_medalists(games_id):
     noc = flask.request.args.get('noc')
     games_id = int(games_id)
     if noc is not None:
-        query = '''SELECT athletes.id, athletes.full_name, athletes.sex, events.sport, events.event_competed, performances.medal
+        query = '''SELECT athletes.id, athletes.full_name, athletes.sex, events.sport, 
+                        events.event_competed, performances.medal
                     FROM performances, games, athletes, events
                     WHERE games.id = %s
                     AND performances.athlete_id = athletes.id
                     AND performances.event_id = events.id
                     AND performances.game_id = games.id
                     AND performances.noc LIKE %s
-                    AND performances.medal LIKE %s;'''
+                    AND performances.medal LIKE %s
+                    ORDER BY athletes.full_name;'''
         try:
             cursor.execute(query, (games_id, noc, '%'))
         except Exception as e:
             print(e)
             exit()
     else:
-        query = '''SELECT athletes.id, athletes.full_name, athletes.sex, events.sport, events.event_competed, performances.medal
+        query = '''SELECT athletes.id, athletes.full_name, athletes.sex, events.sport, 
+                        events.event_competed, performances.medal
                     FROM performances, games, athletes, events
                     WHERE games.id = %s
                     AND performances.athlete_id = athletes.id
                     AND performances.event_id = events.id
                     AND performances.game_id = games.id
-                    AND performances.medal LIKE %s;'''
+                    AND performances.medal LIKE %s
+                    ORDER BY athletes.full_name;'''
         try:
             cursor.execute(query, (games_id,'%'))
         except Exception as e:
             print(e)
             exit()
-    
     medalist_list = []
     for medalist in cursor:
         medalist_info = {'athlete_id':int(medalist[0]), 'athlete_name':medalist[1], \
             'athlete_sex':medalist[2], 'sport':medalist[3], 'event':medalist[4], 'medal':medalist[5]}
         medalist_list.append(medalist_info)
-    
     return json.dumps(medalist_list)
 
-   
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('A sample Flask application/API')
+    parser = argparse.ArgumentParser('An olympic data Flask application/API')
     parser.add_argument('host', help='the host on which this application is running')
     parser.add_argument('port', type=int, help='the port on which this application is listening')
     arguments = parser.parse_args()
